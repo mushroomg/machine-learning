0b6bec977ea0adefd657ddb1e0ef16d2b6098758
Pammi Yeung <mushroomg@users.noreply.github.com>
2017-02-20 18:07:21 -0500

    Making more fixes based on reviewer feedback
    1. Fix state space calculation
    2. Describe sub-optimal policy
    3. Disable createQ and learn while not learning

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -99,7 +99,7 @@ class LearningAgent(Agent):
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
 
-        if (state not in self.Q):
+        if (self.learning and state not in self.Q):
             self.Q[state] = {action: 0.0 for action in self.valid_actions}
 
         return 
@@ -136,7 +136,8 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
-        self.Q[state][action] = (1 - self.alpha)*self.Q[state][action] + self.alpha*reward
+        if self.learning:
+            self.Q[state][action] = (1 - self.alpha)*self.Q[state][action] + self.alpha*reward
         return
 
 
